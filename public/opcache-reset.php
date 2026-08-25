5ee5eeebf8ad9636ee4d7cc91761a4797818abdd
TKCD Deploy <deploy@tkcd.com>
2026-08-25 14:23:50 +0300

    debug: check canli-ara.php version on server

--- public/opcache-reset.php
+++ public/opcache-reset.php
@@ -1,8 +1,6 @@
 <?php
-// GEÇICI - Kullandıktan sonra silin!
-if (function_exists('opcache_reset')) {
-    opcache_reset();
-    echo 'OK';
-} else {
-    echo 'OPcache yok';
-}
+// Hangi canli-ara.php çalışıyor?
+$file = dirname(__DIR__) . '/yonetim/inc/canli-ara.php';
+echo 'Dosya mtime: ' . date('Y-m-d H:i:s', filemtime($file)) . PHP_EOL;
+echo 'ikamet_ilcesi var mi: ' . (strpos(file_get_contents($file), 'ikamet_ilcesi') !== false ? 'EVET' : 'HAYIR') . PHP_EOL;
+echo 'Sunucu dizini: ' . __DIR__;
