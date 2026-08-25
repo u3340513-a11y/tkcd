626c4a9556150957bf2d8eb8bdddc1b9aa88190d
TKCD Deploy <deploy@tkcd.com>
2026-08-25 14:24:53 +0300

    chore: remove debug/opcache helper script

--- public/opcache-reset.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Hangi canli-ara.php çalışıyor?
-$file = dirname(__DIR__) . '/yonetim/inc/canli-ara.php';
-echo 'Dosya mtime: ' . date('Y-m-d H:i:s', filemtime($file)) . PHP_EOL;
-echo 'ikamet_ilcesi var mi: ' . (strpos(file_get_contents($file), 'ikamet_ilcesi') !== false ? 'EVET' : 'HAYIR') . PHP_EOL;
-echo 'Sunucu dizini: ' . __DIR__;
