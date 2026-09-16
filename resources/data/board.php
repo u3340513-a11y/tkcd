3809b606b19bf1c859313b496cc1287c2f9dc595
TKCD Deploy <deploy@tkcd.com>
2026-09-16 21:09:55 +0300

    feat: Büşra Yılmaz fotoğrafı eklendi (büşra-bk.png)

--- resources/data/board.php
+++ resources/data/board.php
@@ -114,7 +114,7 @@ return [
     [
         'baslik' => 'Kadın Kolları',
         'uyeler' => [
-            kisi(slug: 'busra-yilmaz',       ad: 'Büşra Yılmaz',       unvan: 'Kadın Kolları Başkanı'),
+            kisi(slug: 'busra-yilmaz',       ad: 'Büşra Yılmaz',       unvan: 'Kadın Kolları Başkanı', fotograf: 'büşra-bk.png'),
             kisi(slug: 'guluzar-aydogdu',    ad: 'Gülüzar Aydoğdu',    unvan: 'Başkan Yardımcısı'),
             kisi(slug: 'nurcan-degirmenci',  ad: 'Nurcan Değirmenci',   unvan: 'Başkan Yardımcısı'),
             kisi(slug: 'emine-aydin',        ad: 'Emine Aydın',        unvan: 'Başkan Yardımcısı'),
