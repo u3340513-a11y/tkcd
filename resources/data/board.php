d3b9205a31d61c348a9595e2355c05aa21460d89
TKCD Deploy <deploy@tkcd.com>
2026-09-17 22:34:37 +0300

    feat: Uğur Okumuş fotoğrafı güncellendi (ugur-okumus.png)

--- resources/data/board.php
+++ resources/data/board.php
@@ -135,7 +135,7 @@ return [
             kisi(slug: 'mehmet-uzunoglu',         ad: 'Mehmet Uzunoğlu',         unvan: 'Yönetim Kurulu Üyesi'),
             kisi(slug: 'onur-yildiz',             ad: 'Onur Yıldız',             unvan: 'Yönetim Kurulu Üyesi', fotograf: 'onur-yildiz.png'),
             kisi(slug: 'yucel-alp',               ad: 'Yücel Alp',               unvan: 'Yönetim Kurulu Üyesi', fotograf: 'yucel-alp.jpeg'),
-            kisi(slug: 'ugur-okumus',             ad: 'Uğur Okumuş',             unvan: 'Yönetim Kurulu Üyesi'),
+            kisi(slug: 'ugur-okumus',             ad: 'Uğur Okumuş',             unvan: 'Yönetim Kurulu Üyesi', fotograf: 'ugur-okumus.png'),
             kisi(slug: 'salim-suleymanogl',       ad: 'Salim Süleymanoğlu',      unvan: 'Yönetim Kurulu Üyesi'),
             kisi(slug: 'fatma-demir',             ad: 'Fatma Demir',             unvan: 'Yönetim Kurulu Üyesi', fotograf: 'fatma-demir.png'),
         ],
