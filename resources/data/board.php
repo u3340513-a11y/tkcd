2660fcb178ad5af2885daa24ead29f2536033d21
TKCD Deploy <deploy@tkcd.com>
2026-09-17 13:43:26 +0300

    feat: Onur Yıldız fotoğrafı güncellendi (onur-yildiz.png)

--- resources/data/board.php
+++ resources/data/board.php
@@ -133,7 +133,7 @@ return [
             kisi(slug: 'dursun-ali-suleymanogl',  ad: 'Dursun Ali Süleymanoğlu', unvan: 'Yönetim Kurulu Üyesi'),
             kisi(slug: 'ahmet-yilmaz',            ad: 'Ahmet Yılmaz',            unvan: 'Yönetim Kurulu Üyesi'),
             kisi(slug: 'mehmet-uzunoglu',         ad: 'Mehmet Uzunoğlu',         unvan: 'Yönetim Kurulu Üyesi'),
-            kisi(slug: 'onur-yildiz',             ad: 'Onur Yıldız',             unvan: 'Yönetim Kurulu Üyesi'),
+            kisi(slug: 'onur-yildiz',             ad: 'Onur Yıldız',             unvan: 'Yönetim Kurulu Üyesi', fotograf: 'onur-yildiz.png'),
             kisi(slug: 'yucel-alp',               ad: 'Yücel Alp',               unvan: 'Yönetim Kurulu Üyesi', fotograf: 'yucel-alp.jpeg'),
             kisi(slug: 'ugur-okumus',             ad: 'Uğur Okumuş',             unvan: 'Yönetim Kurulu Üyesi'),
             kisi(slug: 'salim-suleymanogl',       ad: 'Salim Süleymanoğlu',      unvan: 'Yönetim Kurulu Üyesi'),
