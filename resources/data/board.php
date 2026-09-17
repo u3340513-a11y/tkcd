fd14fae21d951421f35813af65851da5b348ddaa
TKCD Deploy <deploy@tkcd.com>
2026-09-17 11:52:36 +0300

    feat: Gülüzar Aydoğdu fotoğrafı güncellendi (guluzar-aydogdu.png)

--- resources/data/board.php
+++ resources/data/board.php
@@ -115,7 +115,7 @@ return [
         'baslik' => 'Kadın Kolları',
         'uyeler' => [
             kisi(slug: 'busra-yilmaz',       ad: 'Büşra Yılmaz',       unvan: 'Kadın Kolları Başkanı', fotograf: 'büşra-bk.png'),
-            kisi(slug: 'guluzar-aydogdu',    ad: 'Gülüzar Aydoğdu',    unvan: 'Başkan Yardımcısı'),
+            kisi(slug: 'guluzar-aydogdu',    ad: 'Gülüzar Aydoğdu',    unvan: 'Başkan Yardımcısı', fotograf: 'guluzar-aydogdu.png'),
             kisi(slug: 'nurcan-degirmenci',  ad: 'Nurcan Değirmenci',   unvan: 'Başkan Yardımcısı'),
             kisi(slug: 'emine-aydin',        ad: 'Emine Aydın',        unvan: 'Başkan Yardımcısı'),
         ],
